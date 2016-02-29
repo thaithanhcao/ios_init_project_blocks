80eab715073e506e10d68dd84e434450929cd152
Thai Thanh Cao <apple@apples-MacBook-Pro.local>
2016-02-29 14:09:18 +0700

    - update SimpleTable app.

---
+++ SimpleTablePropertyList/SimpleTable/SimpleTableViewController.m
@@ -0,0 +1,136 @@
+//
+//  SimpleTableViewController.m
+//  SimpleTable
+//
+//  Created by Simon Ng on 16/4/12.
+//  Copyright (c) 2012 AppCoda. All rights reserved.
+//
+
+#import "SimpleTableViewController.h"
+#import "SimpleTableCell.h"
+
+@interface SimpleTableViewController ()
+
+@end
+
+@implementation SimpleTableViewController
+{
+    NSMutableArray *tableData;
+    NSMutableArray *thumbnails;
+    NSMutableArray *prepTime;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Find out the path of recipes.plist
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"recipes" ofType:@"plist"];    
+
+    // Load the file content and read the data into arrays
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+    tableData = [dict objectForKey:@"RecipeName"];
+    thumbnails = [dict objectForKey:@"Thumbnail"];
+    prepTime = [dict objectForKey:@"PrepTime"];
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return [tableData count];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return 78;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *simpleTableIdentifier = @"SimpleTableCell";
+
+    SimpleTableCell *cell = (SimpleTableCell *)[tableView dequeueReusableCellWithIdentifier:simpleTableIdentifier];
+    if (cell == nil) 
+    {
+        NSArray *nib = [[NSBundle mainBundle] loadNibNamed:@"SimpleTableCell" owner:self options:nil];
+        cell = [nib objectAtIndex:0];
+    } 
+    
+    cell.nameLabel.text = [tableData objectAtIndex:indexPath.row];
+    cell.thumbnailImageView.image = [UIImage imageNamed:[thumbnails objectAtIndex:indexPath.row]];
+    cell.prepTimeLabel.text = [prepTime objectAtIndex:indexPath.row];
+    
+    return cell;
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"didSelectRowAtIndexPath");
+    /*UIAlertView *messageAlert = [[UIAlertView alloc]
+                                    initWithTitle:@"Row Selected" message:@"You've selected a row" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];*/
+    UIAlertView *messageAlert = [[UIAlertView alloc]
+                                 initWithTitle:@"Row Selected" message:[tableData objectAtIndex:indexPath.row] delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil];
+    
+    // Display the Hello World Message
+    [messageAlert show];
+    
+    // Checked the selected row
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if (cell.accessoryType == UITableViewCellAccessoryNone)
+    {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    }
+    else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+- (NSIndexPath *)tableView:(UITableView *)tableView willSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    NSLog(@"willSelectRowAtIndexPath");
+    if (indexPath.row == 0) {
+        //return nil;
+    }
+    
+    return indexPath;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
+    // Find out the path of recipes.plist
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"recipes" ofType:@"plist"];    
+    
+    // Load the file content and read the data into arrays
+    NSMutableDictionary *dict = [[NSMutableDictionary alloc] initWithContentsOfFile:path];
+    tableData = [dict objectForKey:@"RecipeName"];
+    thumbnails = [dict objectForKey:@"Thumbnail"];
+    prepTime = [dict objectForKey:@"PrepTime"];
+    
+    // Remove the row from data model
+    [tableData removeObjectAtIndex:indexPath.row];
+    [thumbnails removeObjectAtIndex:indexPath.row];
+    [prepTime removeObjectAtIndex:indexPath.row];
+    
+    
+    [dict writeToFile:path atomically:YES];
+    
+    // Request table view to reload
+    [tableView reloadData];
+}
+
+@end
+
---
+++ SimpleTablePropertyList/SimpleTable/readme.txt
@@ -0,0 +1,5 @@
+﻿SimpleTable module
+Author: Thai Thanh Cao
+
+Description:
+SimpleTable module contains Settings part of the final Swift app - Mobile Online Booking.
